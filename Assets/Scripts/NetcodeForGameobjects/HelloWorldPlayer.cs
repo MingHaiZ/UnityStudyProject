cc859ee224bd207af08892ccb52ad03ca639aa31
MingHai <1163094232@qq.com>
2026-02-09 00:02:57 +0800

    [Update] UpdatePlayer NetworkVariable
    (cherry picked from commit 7d11d61fc053e8d1862ccd2877cd398d0637ae95)

--- Assets/Scripts/NetcodeForGameobjects/HelloWorldPlayer.cs
+++ Assets/Scripts/NetcodeForGameobjects/HelloWorldPlayer.cs
@@ -1,20 +1,45 @@
-﻿using System;
-using Unity.Netcode;
+﻿using Unity.Netcode;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace NetcodeForGameobjects
 {
-    public class HelloWorldPlayer : MonoBehaviour
+    public class HelloWorldPlayer : NetworkBehaviour
     {
-        private NetworkObject m_networkManager;
-
-        private void Start()
+        public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+        
+        public override void OnNetworkSpawn()
         {
-            m_networkManager = GetComponent<NetworkObject>();
+            if (IsOwner)
+            {
+                Move();
+            }
         }
+
         public void Move()
         {
-            Debug.Log("Moveing");
+            SubmitPositionRequestRpc();
+        }
+
+        [Rpc(SendTo.Server)]
+        private void SubmitPositionRequestRpc(RpcParams rpcParams = default)
+        {
+            var randomPosition = GetRandormPositionOnPlane();
+            transform.position = randomPosition;
+            Position.Value = randomPosition;
+        }
+
+        private Vector3 GetRandormPositionOnPlane()
+        {
+            return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
+        }
+
+        private void Update()
+        {
+            if (transform.position != Position.Value)
+            {
+                transform.position = Position.Value;
+            }
         }
     }
 }
